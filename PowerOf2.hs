933d489f4e08906626adc9a3f8e2ab2090ef02fe
Shundo Kishi <syundo0730@gmail.com>
2015-05-27 20:19:00 +0900

    6章 p.307 可視性を制御して性質を保護する
    2の冪乗の整数以外を束縛できないようになった6章 p.307

--- PowerOf2.hs
+++ PowerOf2.hs
@@ -1,7 +1,33 @@
-module PowerOf2 where
+module PowerOf2
+    ( PowerOf2
+    , mkPowerOf2
+    , exponentPowerOf2
+    ) where
 
 -- | 2の冪乗型
 newtype PowerOf2 = PowerOf2 Integer deriving (Eq, Show)
+
+isPowerOf2 :: Integer -> Bool
+isPowerOf2 n
+    | n == 1 = True
+    | n < 1 || odd n = False
+    | otherwise = isPowerOf2 (n `div` 2)
+
+-- | 整数型から2の冪乗型への変換
+--
+-- >>> mkPowerOf2 1
+-- Just (PowerOf2 1)
+-- >>> mkPowerOf2 2
+-- Just (PowerOf2 2)
+-- >>> mkPowerOf2 3
+-- Nothing
+-- >>> mkPowerOf2 1024
+-- Just (PowerOf2 1024)
+
+mkPowerOf2 :: Integer -> Maybe PowerOf2
+mkPowerOf2 n
+    | isPowerOf2 n = Just (PowerOf2 n)
+    | otherwise    = Nothing
 
 -- 2の冪乗に対し、それが2の何乗されたものかを得る
 --
@@ -19,5 +45,4 @@ exponentPowerOf2 (PowerOf2 n) = exponentPowerOf2' 0 n where
     exponentPowerOf2' :: Integer -> Integer -> Integer
     exponentPowerOf2' r n
         | n == 1         = r
-        | n < 1 || odd n = error (shows n " must be a power of 2.")
         | otherwise      = exponentPowerOf2' (r + 1) (n `div` 2)
